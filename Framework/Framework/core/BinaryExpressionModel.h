e148b04b547d5f3abc4273d136be47812f8406bc
Namhto <otenich@gmail.com>
2016-03-10 09:54:11 +0100

    BinaryExpressionModel

--- Framework/Framework/core/BinaryExpressionModel.h
+++ Framework/Framework/core/BinaryExpressionModel.h
@@ -10,22 +10,24 @@ namespace core
 	class BinaryExpressionModel : public BinaryExpression<T> // Interface
 	{
 	private:
-		BinaryExpression<T> op;
-		Expression<T> left, right;
+		BinaryExpression<T>* op;
+		Expression<T>* left, right;
 
 	public:
 		virtual T evaluate() const;
-		virtual T evaluate(Expression<T>* l, Expression<T>* r) const;
+		virtual T evaluate(const Expression<T>*, const Expression<T>*) const;
 	};
 
 	template <class T>
-	T ValueModel<T>::evaluate() const {
-		return T;
+	T BinaryExpressionModel<T>::evaluate() const {
+		if ((left != nullptr) && (right != nullptr))
+			return evaluate(left, right);
 	}
 
 	template <class T>
-	void ValueModel<T>::setValue(const T& _value) {
-		value = _value;
+	T BinaryExpressionModel<T>::evaluate(const Expression<T>* l, const Expression<T>* r) const {
+		if (op != nullptr)
+			return op->evaluate(l, r);
 	}
 }
 #endif
